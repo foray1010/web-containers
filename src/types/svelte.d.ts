8c421d0decf5f407efdf1c45e43d36d7e3d54b5e
Nick Chan <5746214+nickccm1122@users.noreply.github.com>
2019-01-26 19:49:46 +0800

    chore: add manual svelte types

---
+++ src/types/svelte.d.ts
@@ -0,0 +1,29 @@
+declare class Svelte {
+  constructor(options: {target: Element; data?: any; store?: any})
+
+  get(name?: string): any
+  set(data: any): void
+
+  on(eventName: string, callback?: (event?: any) => any): () => {cancel: () => any}
+
+  fire(eventName: string, event?: any): void
+
+  observe(
+    name: string,
+    //@ts-ignore
+    callback: (newValue?, oldValue?) => any,
+    options?: {init?: boolean; defer?: boolean}
+  ): () => {cancel: () => any}
+
+  oncreate(): void
+
+  ondestroy(): void
+
+  destroy(): void
+
+  refs: RefCollection
+}
+
+interface RefCollection {
+  [name: string]: Svelte | HTMLElement
+}
