44e383a7b7901c31646973df28039077f2031106
Nick Chan <5746214+nickccm1122@users.noreply.github.com>
2019-01-26 19:52:37 +0800

    chore: update tsconfig to use manual types

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import testImport from './testImport'
 testImport()
 
 const app = new App({
-  target: document.getElementById('root'),
+  target: document.getElementById('root') as Element,
   data: {
     name: 'world'
   }
