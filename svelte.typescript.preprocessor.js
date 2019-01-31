5140f0f0cbf06b8ae059b06581b20736a84d6cb7
Alex Yeung <foray1010@users.noreply.github.com>
2019-01-31 15:36:41 +0800

    fixup! chore: support typescript in svelte html

--- svelte.typescript.preprocessor.js
+++ svelte.typescript.preprocessor.js
@@ -2,19 +2,13 @@ import fs from 'fs'
 import path from 'path'
 import {promisify} from 'util'
 
+import chalk from 'chalk'
 import * as ts from 'typescript'
 
 import tsconfig from './tsconfig.json'
 
-const readFile = promisify(fs.readFile)
 const removeFile = promisify(fs.unlink)
 const writeFile = promisify(fs.writeFile)
-
-const getTsCompilerOptions = async (configPath) => {
-  const configContents = (await readFile(configPath)).toString()
-  const {config} = ts.parseConfigFileTextToJson(configPath, configContents)
-  return ts.convertCompilerOptionsFromJson(config.compilerOptions || {}, process.cwd())
-}
 
 const renameBaseName = ({filePath, prefix, ext}) => {
   const fileName = path.basename(filePath, path.extname(filePath))
@@ -31,7 +25,10 @@ export default async ({content, filename: filePath}) => {
   const tmpFilePath = renameBaseName({filePath, prefix: '.svelte-typescript.', ext: '.ts'})
   await writeFile(tmpFilePath, content)
 
-  const compilerOptions = await getTsCompilerOptions('./tsconfig.json')
+  const compilerOptions = ts.convertCompilerOptionsFromJson(
+    tsconfig.compilerOptions || {},
+    process.cwd()
+  )
 
   const program = ts.createProgram([tmpFilePath], {
     ...compilerOptions.options,
@@ -42,7 +39,12 @@ export default async ({content, filename: filePath}) => {
   const emitResult = program.emit()
 
   const allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics)
-  console.log('allDiagnostics', allDiagnostics)
+  allDiagnostics.forEach((diagnostic) => {
+    const fileName = path.relative(process.cwd(), filePath)
+    console.error(
+      chalk.red(`${fileName}: ${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`)
+    )
+  })
 
   // cleanup
   await removeFile(tmpFilePath)
