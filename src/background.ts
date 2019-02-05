142adc1c2b1c4fe5a02c3f87ada086216683ec30
Alex Yeung <foray1010@users.noreply.github.com>
2019-02-05 14:56:14 +0800

    feat: block webrequest on target domains

--- src/background.ts
+++ src/background.ts
@@ -1,23 +1,36 @@
 import '@babel/polyfill'
 
 import {clearDomainCookies, setupContainer} from './services/container'
+import convertDomainToRegExp from './utils/convertDomainToRegExp'
 
 // TODO: testing, can remove
-const TWITTER_DOMAINS = [
-  'twitter.com',
-  'www.twitter.com',
-  't.co',
-  'twimg.com',
-  'mobile.twitter.com',
-  'm.twitter.com',
-  'api.twitter.com',
-  'abs.twimg.com',
-  'ton.twimg.com',
-  'pbs.twimg.com',
-  'tweetdeck.twitter.com'
-]
+const TWITTER_DOMAINS = ['*.t.co', '*.twimg.com', '*.twitter.com']
+const TWITTER_DOMAIN_REGEXPS = TWITTER_DOMAINS.map(convertDomainToRegExp)
+console.debug('TWITTER_DOMAIN_REGEXPS', TWITTER_DOMAIN_REGEXPS)
 
-async function init() {
+browser.webRequest.onBeforeRequest.addListener(
+  (details) => {
+    console.debug('onBeforeRequest request', {
+      'details.url': details.url,
+      'new URL(details.url).hostname': new URL(details.url).hostname
+    })
+
+    const response = {
+      cancel: TWITTER_DOMAIN_REGEXPS.some((regexp) => {
+        return regexp.test(new URL(details.url).hostname)
+      })
+    }
+    console.debug('onBeforeRequest response', response)
+    return response
+  },
+  {
+    types: ['main_frame'],
+    urls: ['http://*/*', 'https://*/*']
+  },
+  ['blocking']
+)
+
+async function init(): Promise<void> {
   const cookieStoreId = await setupContainer({
     name: 'Testing Twitter'
   })
