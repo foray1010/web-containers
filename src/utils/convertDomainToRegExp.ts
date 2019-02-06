31ce57bc05eb0f88a9863c58bb236326157574e5
Alex Yeung <foray1010@users.noreply.github.com>
2019-02-06 03:41:01 +0000

    feat: block webrequest on target domains (#50)

---
+++ src/utils/convertDomainToRegExp.ts
@@ -0,0 +1,5 @@
+import escapeStringRegexp from 'escape-string-regexp'
+
+export default (domain: string): RegExp => {
+  return new RegExp('^' + escapeStringRegexp(domain).replace(/^\\\*\\\./, '(.+\\.)?') + '$', 'i')
+}
