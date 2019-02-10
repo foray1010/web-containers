b87f8502f3bee8a375af7b039724975ada10801f
Alex Young <foray1010@users.noreply.github.com>
2019-02-10 22:25:31 +0800

    feat: reopen tab in container

---
+++ src/services/tabs.ts
@@ -0,0 +1,23 @@
+export const reopenTabInContainer = async (
+  tabId: number,
+  cookieStoreId: string,
+  overwriteUrl?: string
+): Promise<void> => {
+  const tab = await browser.tabs.get(tabId)
+  console.debug('tab', tab)
+
+  const url = overwriteUrl || tab.url
+  console.debug(`reopen ${url} in ${cookieStoreId}`)
+
+  await browser.tabs.create({
+    active: tab.active,
+    cookieStoreId,
+    index: tab.index,
+    url,
+    windowId: tab.windowId
+  })
+
+  if (tab.id !== undefined) {
+    await browser.tabs.remove(tab.id)
+  }
+}
