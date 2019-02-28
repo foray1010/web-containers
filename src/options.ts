1cd6a1cd5e5f5b0f6a2c5c42d96261753934131b
Alex Young <foray1010@users.noreply.github.com>
2019-02-28 21:20:22 +0800

    feat(options): save my rules when submit (#98)

--- src/options.ts
+++ src/options.ts
@@ -3,15 +3,27 @@ import '@babel/polyfill'
 import OptionsApp from './components/OptionsApp.html'
 import {CONTAINER_COLORS, CONTAINER_ICONS} from './constants/container'
 import presets from './presets.json'
+import {getSyncConfigs} from './services/configurations'
 import valuesFromEnum from './utils/valuesFromEnum'
 
-new OptionsApp({
+const optionsApp = new OptionsApp({
   target: document.getElementById('root') as Element,
   data: {
     containerColors: valuesFromEnum(CONTAINER_COLORS),
     containerIcons: valuesFromEnum(CONTAINER_ICONS),
-    // TODO: get it from options
-    myRules: presets,
     presets
   }
+})
+
+const main = async (): Promise<void> => {
+  const syncConfig = await getSyncConfigs()
+  if (syncConfig.containerConfigs) {
+    optionsApp.set({
+      myRules: syncConfig.containerConfigs
+    })
+  }
+}
+
+main().catch((err) => {
+  console.error(err)
 })
